73e48415e32c02055d7bb6e9271277415657b7fe
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-23 14:03:06 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 import Navbar from "./Components/navbar/Navbar";
 import Menu from "./Components/menu/Menu";
 import Portfolio from './Components/portfolio/Portfolio'
+import Work from './Components/work/Work';
+import Testimonial from './Components/testimonial/Testimonial';
+import Contact from './Components/contact/Contact';
+import Intro from './Components/intro/Intro';
 
 
 
@@ -13,7 +17,11 @@ function App() {
      <Navbar menuBar={menuBar} setMenuBar={setMenuBar}/>
      <Menu  menuBar={menuBar}  setMenuBar={setMenuBar}/>
      <div className='sections'>
+     <Intro/>
      <Portfolio/>
+     <Work/>
+     <Testimonial/>
+     <Contact/>
      
      </div>
     </div>
---
+++ src/Components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import '../intro/intro.scss'
+
+function Intro() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Intro
