0912fe7d1a01ec2d9cfdd56f2c8ffca628780c2d
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-23 02:52:59 +0100

    new

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -11,12 +11,18 @@ function Navbar() {
     <div className='item-container'>
        <PhoneIcon className='icon'/>
        <span>08097407765</span>
+       </div>
+       <div className='item-container'>
        <MailIcon className='icon'/>
        <span><a href="https://mail.google.com/mail/u/0/#inbox">cynthiachiagu@gmail.com</a></span>
 
     </div>
     </div>
-    <div className='right'></div>
+    <div className='right'>
+      <span className='line1'></span>
+      <span className='line2'></span>
+      <span className='line3'></span>
+    </div>
        
     </div>
       
