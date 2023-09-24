fdbd472a0a4b9b42d7ef57e0a8e7699c4eacd0ec
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-24 12:55:01 +0100

    new

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -3,15 +3,19 @@ import '../intro/intro.scss'
 function Intro() {
   return (
     <div className='intro' id='intro'>
-    <div className='container'>
-    <div className='left'>
-      <img src='Assets/cyndy.png'/>
-    </div>
-    <div className='right'></div>
+     <div className='left'>
+       <div className='img-container'>
+        <img src='Assets/cyndy.png'alt=''/>
+       </div>
+     </div> 
 
-    
-    </div>
-      
+     <div className='right'>
+      <div className='right-content'>
+       <h2>Hi There , I'm</h2>
+       <h1>Cynthia</h1>
+       <h3>Freelance</h3>
+      </div>
+     </div>   
     </div>
   )
 }
