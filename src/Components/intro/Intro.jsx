4d81fa31ad26f8e2fd873fe68a6cbb4f0ee0a864
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-24 14:07:45 +0100

    new

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useRef } from 'react'
 import '../intro/intro.scss'
+import {init} from 'ityped'
 
 function Intro() {
+  const text = useRef()
+   
+  useEffect(()=>{
+    init(text.current,{
+      backDelay:2000,
+      showCursor:true,
+      backSpeed:60,
+      strings:['frontend designer..','product designer', 'UIUX designer']
+
+    },[])
+  })
   return (
     <div className='intro' id='intro'>
      <div className='left'>
@@ -13,7 +26,7 @@ function Intro() {
       <div className='right-content'>
        <h2>Hi There , I'm</h2>
        <h1>Cynthia</h1>
-       <h3>Freelance</h3>
+       <h3>Freelance  <span ref={text}></span></h3>
       </div>
      </div>   
     </div>
