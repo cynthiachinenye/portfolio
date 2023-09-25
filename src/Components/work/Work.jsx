8d1a3d0321035e1813d6b3ecf553c0d827ea6356
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-25 13:25:31 +0100

    new

--- src/Components/work/Work.jsx
+++ src/Components/work/Work.jsx
@@ -1,6 +1,6 @@
 import '../work/work.scss'
 import { useState } from 'react'
-import {work} from '../../WorkList'
+import { work } from '../../WorkList'
 
 function Work() {
   const [slides, setSlides] = useState([])
@@ -8,35 +8,41 @@ function Work() {
 
   return (
     <div className='work' id="work">
-    <div className='slides' style={{transform:`translate (${slides * 100}vw)`}}>
-       {
-        work.map((data)=>(
-         
-          
-          <div className='containter'>
-    <div className='left'>
-      <div className='icons'>
-      <img src={data.icon} alt='' />
+      <div className='slides' style={{ transform: `translate (${slides * 100}vw)` }}>
+        {
+          work.map((data) => (
+
+
+            <div className='container'>
+              <div className='item'>
+              
+              <div className='left'>
+             <div className='left-container'>
+             <div className='img-icons'>
+             <img src={data.icon} alt=''/>
+           </div>
+           <h2>{data.title}</h2>
+           <p>{data.desc}</p>
+
+           <span>Projects</span>
+             </div>
+            </div> 
+
+              </div>
+
+              <div className='right'>
+                <img src={data.img} />
+              </div>
+
+            </div>
+
+          ))
+        }
+
+
       </div>
-      <h2>{data.title}</h2>
-      <p>{data.desc}</p>
 
-      <span>Projects</span>
-    </div>
 
-    <div className='right'>
-     <img src={data.img}/>
-    </div>
-
-    </div>
-    
-        ))
-       }
-    
-    
-    </div>
-    
-      
     </div>
   )
 }
