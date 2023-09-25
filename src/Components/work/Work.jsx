57719abc39b1e2e5796099cc96e1c5775510588d
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-25 12:45:30 +0100

    new

--- src/Components/work/Work.jsx
+++ src/Components/work/Work.jsx
@@ -1,8 +1,38 @@
 import '../work/work.scss'
+import { useState } from 'react'
+import {WorkList} from '../../WorkList'
 
 function Work() {
+  const [slides, setSlides] = useState([])
+
+
   return (
-    <div className='work'>
+    <div className='work' id="work">
+    <div className='slides' style={{transform:`translate (${slides * 100}vw)`}}>
+       {
+        WorkList.map((data)=>(
+         
+          
+          <div className='containter'>
+    <div className='left'>
+      <div className='icons'>
+      <img src={data.icon} alt='' />
+      </div>
+      <p></p>
+
+      <h3>Projects</h3>
+    </div>
+
+    <div className='right'></div>
+
+    </div>
+    
+        ))
+       }
+    
+    
+    </div>
+    
       
     </div>
   )
---
+++ src/WorkList.jsx
@@ -0,0 +1,39 @@
+
+export const work = [
+
+    {
+        id:1,
+        title:'Web Design',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus vehicula pretium lacus, aliquam mattis nulla condimentum eget. Sed vel lorem et odio iaculis volutpat. Nulla ultrices imperdiet leo vitae rutrum',
+        img:'https://cdn.dribbble.com/userupload/10064562/file/still-9afcc29853b3b46b864a05045832ad81.png?resize=640x480&vertical=center',
+        icon:'Assets/world.png',
+    },
+
+    {
+        id:2,
+        title:'content',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus vehicula pretium lacus, aliquam mattis nulla condimentum eget. Sed vel lorem et odio iaculis volutpat. Nulla ultrices imperdiet leo vitae rutrum',
+        img:'https://cdn.dribbble.com/userupload/10117765/file/still-7fc5611410d27f6f860c5d39641df917.png?resize=450x338&vertical=center',
+        icon:'Assets/writing.png',
+
+    },
+
+    {
+        id:3,
+        title:'Product',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus vehicula pretium lacus, aliquam mattis nulla condimentum eget. Sed vel lorem et odio iaculis volutpat. Nulla ultrices imperdiet leo vitae rutrum',
+        img:'https://cdn.dribbble.com/userupload/10117765/file/still-7fc5611410d27f6f860c5d39641df917.png?resize=450x338&vertical=center',
+        icon:'Assets/phone2.png',
+
+    },
+    {
+        id:4,
+        title:'Mobile',
+        desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus vehicula pretium lacus, aliquam mattis nulla condimentum eget. Sed vel lorem et odio iaculis volutpat. Nulla ultrices imperdiet leo vitae rutrum',
+        img:'https://cdn.dribbble.com/userupload/10117765/file/still-7fc5611410d27f6f860c5d39641df917.png?resize=450x338&vertical=center',
+        icon:'Assets/phone2.png',
+
+    },
+
+
+]
