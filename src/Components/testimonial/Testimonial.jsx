d81eba9fe547441254a8e132ffd99da40895e5dd
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-27 12:00:30 +0100

    new

--- src/Components/testimonial/Testimonial.jsx
+++ src/Components/testimonial/Testimonial.jsx
@@ -1,12 +1,35 @@
 import '../testimonial/testimonail.scss'
+import {test} from '../../Test'
+
 
 function Testimonial() {
   return (
-    <div className='testimonial' id='testimonial'>
+    <div className='test' id='testimonial'>
     <h1> Testimonial</h1>
-    <div className=''></div>
+    <div className='containers'>
+    {
+      test.map((test)=>(
+    <div className={test.features ?' card features ' : 'card'}>
+       <div className='top'>
+       <img src='Assets/arrow-key.png' alt='' className='left'/>
+       <img src={test.img} alt='' className='center'/>
+       <img src={test.icon} alt='' className='right'/>
+
+       </div>
+       <div className='center'>{test.desc}</div>
+       <div className='bottom'>
+       <h3>{test.name}</h3>
+       <h4>{test.title}</h4>
+       </div>
+
+    
+    </div>
+    ))
+  }
+    </div>
       
     </div>
+    
   )
 }
 
---
+++ src/Test.jsx
@@ -0,0 +1,36 @@
+
+  
+
+  export const test = [
+    {
+        id:'1',
+        name:'Tom Durden',
+        title:'Senior Developer',
+        img:'https://images.pexels.com/photos/7675004/pexels-photo-7675004.jpeg?auto=compress&cs=tinysrgb&w=600',
+        icon:'Assets/twitter.png',
+        desc:'Lorem ipsum dolor sit amet consectetur adipiscing elit Nunc vitae mi diam Vestibulum ante ipsum primis',
+
+    },    
+
+    {
+        id:'2',
+        name:'Micheal',
+        title:'FullStack Developer',
+        img:'https://images.pexels.com/photos/3153199/pexels-photo-3153199.jpeg?auto=compress&cs=tinysrgb&w=600',
+        icon:'Assets/youtube.png',
+        desc:'Lorem ipsum dolor sit amet consectetur adipiscing elit Nunc vitae mi diam Vestibulum ante ipsum primis',
+        featured:true,
+    },    
+
+    {
+        id:'3',
+        name:'Alexender Durden',
+        title:'CEO OF LAMA',
+        img:'https://images.pexels.com/photos/18125671/pexels-photo-18125671/free-photo-of-sexy-woman-posing-in-dark.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+        icon:'Assets/Linkedin.png',
+        desc:'Lorem ipsum dolor sit amet consectetur adipiscing elit Nunc vitae mi diam Vestibulum ante ipsum primis',
+      
+    },    
+
+    
+  ]
