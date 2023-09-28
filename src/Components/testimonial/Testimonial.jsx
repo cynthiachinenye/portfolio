0e10e0d28be5d15c46ce345bdcf76a6a5a9b9b44
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-28 12:46:30 +0100

    new

--- src/Components/testimonial/Testimonial.jsx
+++ src/Components/testimonial/Testimonial.jsx
@@ -1,35 +1,35 @@
 import '../testimonial/testimonail.scss'
-import {test} from '../../Test'
+import { test } from '../../Test'
 
 
 function Testimonial() {
   return (
     <div className='test' id='testimonial'>
-    <h1> Testimonial</h1>
-    <div className='containers'>
-    {
-      test.map((test)=>(
-    <div className={test.features ?' card features ' : 'card'}>
-       <div className='top'>
-       <img src='Assets/arrow-key.png' alt='' className='left'/>
-       <img src={test.img} alt='' className='center'/>
-       <img src={test.icon} alt='' className='right'/>
+      <h1> Testimonial</h1>
+      <div className='containers'>
+        {
+          test.map((test) => (
+            <div className={test.featured ? ' card featured ' : 'card'}>
+              <div className='top'>
+                <img src='Assets/arrow-key.png' alt='' className='left' />
+                <img src={test.img} alt='' className='user' />
+                <img src={test.icon} alt='' className='right' />
 
-       </div>
-       <div className='center'>{test.desc}</div>
-       <div className='bottom'>
-       <h3>{test.name}</h3>
-       <h4>{test.title}</h4>
-       </div>
+              </div>
+              <div className='center'>{test.desc}</div>
+              <div className='bottom'>
+                <h3>{test.name}</h3>
+                <h4>{test.title}</h4>
+              </div>
 
-    
+
+            </div>
+          ))
+        }
+      </div>
+
     </div>
-    ))
-  }
-    </div>
-      
-    </div>
-    
+
   )
 }
 
