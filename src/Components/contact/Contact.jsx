fc735d59c8376ecaf50d9a30ea62f5f28d07f81f
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-28 18:58:24 +0100

    new

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -2,7 +2,23 @@ import '../contact/contact.scss'
 
 function Contact() {
   return (
-    <div className='contact'>
+    <div className='contact'  id='contact' >
+    <div className='container'>
+       
+    <div className='left'>
+    <img src="https://w7.pngwing.com/pngs/847/280/png-transparent-two-person-shaking-hands-handshake-handshake-hands-love-hand-people-thumbnail.png" alt=''/>
+    </div>
+    <div className='right'>
+     <h1>Contact</h1>
+     <form >
+     <input type='email' placeholder='enter your mail'/>
+     <textarea  placeholder='Message'></textarea>
+     <button type='submit'>Submit</button>
+     
+     </form>
+     
+    </div>
+    </div>
       
     </div>
   )
